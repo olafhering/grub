32783d07999747dd32a2ca6857ec2461f352a60d
Colin Watson <cjwatson@canonical.com>
2013-01-02 16:52:59 +0000

    * Merge from Ubuntu:   - Don't permit loading modules on UEFI Secure Boot (since in such a     setup the GRUB core image must be signed but it has no provision for     verifying module signatures).

--- grub-core/kern/dl.c
+++ grub-core/kern/dl.c
@@ -42,6 +42,10 @@
 #include <sys/mman.h>
 #endif
 
+#ifdef GRUB_MACHINE_EFI
+#include <grub/efi/efi.h>
+#endif
+
 
 
 #pragma GCC diagnostic ignored "-Wcast-align"
@@ -667,6 +671,15 @@ grub_dl_load_file (const char *filename)
   grub_ssize_t size;
   void *core = 0;
   grub_dl_t mod = 0;
+
+#ifdef GRUB_MACHINE_EFI
+  if (grub_efi_secure_boot ())
+    {
+      grub_error (GRUB_ERR_ACCESS_DENIED,
+		  "Secure Boot forbids loading module from %s", filename);
+      return 0;
+    }
+#endif
 
   file = grub_file_open (filename);
   if (! file)
