c79d05703ec41571c44a0a09df31cf220d951c09
Colin Watson <cjwatson@debian.org>
2014-01-13 12:12:50 +0000

    Hack prefix for OLPC
    This sucks, but it's better than what OFW was giving us.
    
    Patch-Name: olpc_prefix_hack.patch

--- grub-core/kern/ieee1275/init.c
+++ grub-core/kern/ieee1275/init.c
@@ -65,6 +65,7 @@ grub_exit (void)
   grub_ieee1275_exit ();
 }
 
+#ifndef __i386__
 /* Translate an OF filesystem path (separated by backslashes), into a GRUB
    path (separated by forward slashes).  */
 static void
@@ -79,10 +80,19 @@ grub_translate_ieee1275_path (char *filepath)
       backslash = grub_strchr (filepath, '\\');
     }
 }
+#endif
 
 void (*grub_ieee1275_net_config) (const char *dev,
 				  char **device,
 				  char **path);
+#ifdef __i386__
+void
+grub_machine_get_bootlocation (char **device __attribute__ ((unused)),
+			       char **path __attribute__ ((unused)))
+{
+  grub_env_set ("prefix", "(sd,1)/");
+}
+#else
 void
 grub_machine_get_bootlocation (char **device, char **path)
 {
@@ -150,6 +160,7 @@ grub_machine_get_bootlocation (char **device, char **path)
     }
   grub_free (bootpath);
 }
+#endif
 
 /* Claim some available memory in the first /memory node. */
 #ifdef __sparc__
