96bd171544bc5ba6c7665013fbd2231a89453e6e
Peter Jones <pjones@redhat.com>
2019-04-24 10:03:04 -0400

    Forbid the "devicetree" command when Secure Boot is enabled.
    Signed-off-by: Peter Jones <pjones@redhat.com>
    Signed-off-by: Steve McIntyre <93sam@debian.org>
    
    Origin: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=927888#15
    Bug-Debian: https://bugs.debian.org/927888
    Last-Update: 2019-05-04
    
    Patch-Name: no-devicetree-if-secure-boot.patch

--- grub-core/loader/efi/fdt.c
+++ grub-core/loader/efi/fdt.c
@@ -123,6 +123,14 @@ grub_cmd_devicetree (grub_command_t cmd __attribute__ ((unused)),
       return GRUB_ERR_NONE;
     }
 
+#ifdef GRUB_MACHINE_EFI
+  if (grub_efi_secure_boot ())
+    {
+      return grub_error (GRUB_ERR_ACCESS_DENIED,
+		  "Secure Boot forbids loading devicetree from %s", argv[0]);
+    }
+#endif
+
   dtb = grub_file_open (argv[0], GRUB_FILE_TYPE_DEVICE_TREE_IMAGE);
   if (!dtb)
     goto out;
