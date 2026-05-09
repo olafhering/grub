304ba2637a6f6a7ffb0bcf32ec75911cbba787c4
Andrew Hamilton <adhamilt@gmail.com>
2026-05-09 09:33:09 -0500

    CI: Add parallel make for building
    Add parallel make for building GRUB in the CI process.
    
    Signed-off-by: Andrew Hamilton <adhamilt@gmail.com>
    Reviewed-By: Leo Sandoval <lsandova@redhat.com>

--- .gitlab-ci/script/build.sh
+++ .gitlab-ci/script/build.sh
@@ -2,6 +2,14 @@
 
 set -eu -o pipefail
 
+# Set the number of parallel jobs for make, defaulting to 1 if it cannot be determined
+CPU_COUNT=1
+
+if command -v nproc >/dev/null 2>&1
+then
+    CPU_COUNT=$(nproc 2>/dev/null)
+fi
+
 PATH="$CROSS_C/ia64-linux/bin:$CROSS_C/loongarch64-linux/bin:$CROSS_C/riscv32-linux/bin:$PATH"
 i="${TARGET}"
 j="${PLATFORM}"
@@ -12,6 +20,6 @@ else
   ./configure --target="$i" --with-platform="$j" --enable-grub-mkfont --prefix="$(pwd)/grub-dist"
 fi
 
-make --quiet install
+make --quiet -j"${CPU_COUNT}" install
 make --quiet html
 make --quiet pdf
