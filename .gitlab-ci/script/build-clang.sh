4482637b4b8796154735cfb9523e6616b7ebacf0
Leo Sandoval <lsandova@redhat.com>
2026-07-08 12:39:42 -0600

    CI: increase clang build platform coverage on x86_64 target
    Although many more target/platforms pairs are missing, the following
    change tries to increase the build coverage on x86_64 platforms.
    
    Signed-off-by: Leo Sandoval <lsandova@redhat.com>
    Reviewed-by: Andrew Hamilton <adhamilt@gmail.com>
    Reviewed-by: Sudhakar Kuppusamy <sudhakar@linux.ibm.com>
    Reviewed-by: Avnish Chouhan <avnish@linux.ibm.com>
    Part-of: <https://gitlab.freedesktop.org/gnu-grub/grub/-/merge_requests/180>

--- .gitlab-ci/script/build-clang.sh
+++ .gitlab-ci/script/build-clang.sh
@@ -5,8 +5,15 @@ set -eu -o pipefail
 T="${TARGET}"
 P="${PLATFORM}"
 
-CC=clang BUILD_CC=clang TARGET_CC=clang HOST_CC=clang \
-	 ./configure --target="$T" --with-platform="$P" \
-	 --enable-stack-protector --enable-grub-mkfont --prefix="$(pwd)/grub-dist"
+
+if [ "$P" = efi ]; then
+    CC=clang BUILD_CC=clang TARGET_CC=clang HOST_CC=clang \
+             ./configure --target="$T" --with-platform="$P" \
+             --enable-stack-protector --enable-grub-mkfont --prefix="$(pwd)/grub-dist"
+else
+    CC=clang BUILD_CC=clang TARGET_CC=clang HOST_CC=clang \
+             ./configure --target="$T" --with-platform="$P" \
+             --enable-grub-mkfont --prefix="$(pwd)/grub-dist"
+fi
 
 make --quiet install
